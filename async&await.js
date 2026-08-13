e18d8be0b619d27ce6a1d431b4e5d6ca21b77e37
TousifX <mr.trexyt9@gmail.com>
2026-08-13 14:07:40 +0530

    updared

--- async&await.js
+++ async&await.js
@@ -1,4 +1,22 @@
-async function greet() {
+const head = document.getElementById("Heading");
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      head.style.color = color;
+      console.log(`color change to ${color}`);
+      resolve("color changed");
+    }, delay);
+  });
+}
+async function demo() {
+  await changeColor("red", 1000);
+  await changeColor("pink", 1000);
+  await changeColor("blue", 1000);
+  await changeColor("yellow", 1000);
+}
+
+/*async function greet() {
   //   throw "error 404";
   return "hello";
 }
@@ -9,4 +27,20 @@ greet()
   })
   .catch((err) => {
     console.log("promise was rejected :", err);
+  });*/
+/*function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+      resolve();
+    }, 1000);
   });
+}
+
+async function demo() {
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+}*/
