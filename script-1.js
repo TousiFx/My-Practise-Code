5c8c1f80c1018479d286c792e83054032e9bd3de
TousifX <mr.trexyt9@gmail.com>
2026-08-15 12:58:02 +0530

    updates

--- script-1.js
+++ script-1.js
@@ -2,37 +2,37 @@ const somaValue = require("./math");
 
 console.log(somaValue);
 
-// const head = document.getElementById("Heading");
+const head = document.getElementById("Heading");
 
-// function changeColor(color, delay) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const ranNum = Math.floor(Math.random() * 5) + 1;
-//       if (ranNum > 3) {
-//         reject("Promise Got Rejected. Please try again. ");
-//       }
-//       head.style.color = color;
-//       console.log(`color change to ${color}`);
-//       resolve("Promises Fulfilled ");
-//     }, delay);
-//   });
-// }
-// async function demo() {
-//   try {
-//     await changeColor("red", 1000);
-//     await changeColor("green", 1000);
-//     await changeColor("blue", 1000);
-//     await changeColor("purple", 1000);
-//     console.log("");
-//   } catch (err) {
-//     console.log("Error caught.");
-//     console.log(err);
-//   }
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const ranNum = Math.floor(Math.random() * 5) + 1;
+      if (ranNum > 3) {
+        reject("Promise Got Rejected. Please try again. ");
+      }
+      head.style.color = color;
+      console.log(`color change to ${color}`);
+      resolve("Promises Fulfilled ");
+    }, delay);
+  });
+}
+async function demo() {
+  try {
+    await changeColor("red", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("purple", 1000);
+    console.log("");
+  } catch (err) {
+    console.log("Error caught.");
+    console.log(err);
+  }
 
-//   let a = 5;
-//   console.log(a);
-//   console.log("New Number :", a + 3);
-// }
+  let a = 5;
+  console.log(a);
+  console.log("New Number :", a + 3);
+}
 
 /*async function greet() {
   //   throw "error 404";
