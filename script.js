942993f3c8a40d4408e8b0b95a5012706e4b2c87
TousiFx <mr.trexyt9@gmail.com>
2026-08-18 11:22:19 +0530

    change

---
+++ script.js
@@ -0,0 +1,7 @@
+const apple = require("./list-item/apple");
+const banana = require("./list-item/banana");
+const orange = require("./list-item/banana");
+
+let fuite = [apple, banana, orange];
+
+console.log(fuite);
